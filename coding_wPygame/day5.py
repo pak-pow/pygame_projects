33f6cee7708a194581b49b9c71f1cabd29eb9019
vince <paulag10987@gmail.com>
2025-12-02 19:44:19 +0800

    feat(day5): add player class

--- coding_wPygame/day5.py
+++ coding_wPygame/day5.py
@@ -1,8 +1,6 @@
 """
 DAY 5 - learning how to use sprites
 """
-from symtable import Class
-
 # imports
 
 import pygame
@@ -17,9 +15,36 @@ from pygame.locals import *
 class Player(Sprite):
 
     def __init__(self):
+
+        # initializing the parent class from the SPRITE
+        # class in pygame
         super().__init__()
 
-    pass
+        # creating the user object
+        self.PLAYER_OBJ = pygame.Surface((50,50))
+
+        # fill it with this color
+        self.PLAYER_OBJ.fill((0,0,255))
+
+        # placing it in a position
+        self.PLAYER_RECT = self.PLAYER_OBJ.get_rect()
+        self.PLAYER_RECT.center = (300,500)
+
+        # Adding physics to the player object
+        self.PLAYER_POS_X = float(self.PLAYER_RECT.x)
+        self.SPEED = 300
+
+    # update function for every time a user presses
+    def update(self, dt):
+
+        # returns a list of what key is pressed
+        keys = pygame.key.get_pressed()
+
+        if (keys[K_LEFT] or keys[K_d]) and self.PLAYER_RECT.left > 0:
+            self.PLAYER_POS_X -= self.SPEED * dt
+
+        if (keys[K_RIGHT] or keys[K_a]) and self.PLAYER_RECT.left > 0:
+            self.PLAYER_POS_X -= self.SPEED * dt
 
 def main():
 
@@ -27,7 +52,9 @@ def main():
 
     WINDOW_WIDTH = 600
     WINDOW_HEIGHT = 500
+
     DISPLAY = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    DISPLAY_COLOR = (255,255,255)
 
     while True:
 
@@ -37,7 +64,7 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        DISPLAY.fill((255,255,255))
+        DISPLAY.fill(DISPLAY_COLOR)
         pygame.display.update()
 
 if __name__ == "__main__":
