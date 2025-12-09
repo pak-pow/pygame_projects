90b75cd472b5f032e6af50607973ac3626f73490
vince <paulag10987@gmail.com>
2025-12-09 16:53:27 +0800

    chore(day12): add base code

---
+++ coding_wPygame/day12.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+import random
+
+from pygame.locals import *
+
+class Particle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+    def update(self):
+        pass
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = Main()
+    app.run()
