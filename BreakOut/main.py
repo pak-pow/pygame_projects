67dd574341553d151dbdf82d40a038ac28d41034
vince <paulag10987@gmail.com>
2025-12-13 14:36:20 +0800

    chore(main.py): add tile function and level map ascii

--- BreakOut/main.py
+++ BreakOut/main.py
@@ -3,16 +3,79 @@ import sys
 
 from pygame.locals import *
 
+
+LEVEL_MAP = [
+    "XXXXXXXXXXXXXXX",  # 0  Top wall
+    "XBBBBBBBBBBBBBX",  # 1
+    "XBBBBBBBBBBBBBX",  # 2
+    "XBBBBBBBBBBBBBX",  # 3
+    "XBBBBBBBBBBBBBX",  # 4
+    "XBBBBBBBBBBBBBX",  # 5
+    "X             X",  # 6
+    "X             X",  # 7
+    "X             X",  # 8
+    "X             X",  # 9
+    "X             X",  # 10
+    "X             X",  # 11
+    "X             X",  # 12
+    "X             X",  # 13
+    "X             X",  # 14
+    "X             X",  # 15
+    "X      O      X",  # 16 Ball spawn
+    "X             X",  # 17
+    "X    PPPPP    X",  # 18 Paddle
+    "XXXXXXXXXXXXXXX",  # 19 Bottom wall
+]
+
+
+class Tile(pygame.sprite.Sprite):
+
+    def __init__(self, pos, type):
+        super().__init__()
+
+        self.image = pygame.Surface((Main.TILE_SIZE, Main.TILE_SIZE))
+
+        if type == "X":
+            self.image.fill((180,180,180))
+            pygame.draw.rect(
+                self.image,
+                (80,80,80),
+                (0,0, Main.TILE_SIZE, Main.TILE_SIZE),
+                3
+            )
+
+        elif type == "B":
+            self.image.fill((0,150,255))
+            pygame.draw.rect(
+                self.image,
+                (0,80,250),
+                self.image.get_rect(),
+                2
+            )
+
+        elif type == "P":
+            self.image.fill((0,255,0))
+
+        elif type == "O":
+            self.image.fill((255,255,0))
+
+        else:
+            self.kill()
+
+
+
+
 class Main:
+
+    TILE_SIZE = 40
+    DISPLAY_WIDTH = 600
+    DISPLAY_HEIGHT = 800
+    DISPLAY_COLOR = (0,0,0)
+    DISPLAY = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+
     def __init__(self):
 
         pygame.init()
-
-        self.DISPLAY_WIDTH = 800
-        self.DISPLAY_HEIGHT = 600
-        self.DISPLAY_COLOR = (255,255,255)
-        self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
-
         self.CLOCK = pygame.time.Clock()
         self.FPS = 60
 
