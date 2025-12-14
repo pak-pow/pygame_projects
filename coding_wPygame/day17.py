b0be9084d934f00dfd66a4e75f87f8d1f931ebc3
vince <paulag10987@gmail.com>
2025-12-14 14:28:54 +0800

    chore(day17.py): add base file code

---
+++ coding_wPygame/day17.py
@@ -0,0 +1,16 @@
+import pygame
+import sys
+import json
+
+from pygame.locals import *
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = Main()
+    app.run()
