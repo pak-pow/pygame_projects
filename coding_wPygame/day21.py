4643457362d57bc9b6b597f6590e23ea389fb73f
vince <paulag10987@gmail.com>
2025-12-18 13:46:17 +0800

    chore(day21): add day21 base code

---
+++ coding_wPygame/day21.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+from pygame.locals import *
+
+class Main:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    app = Main()
+    app.run()
