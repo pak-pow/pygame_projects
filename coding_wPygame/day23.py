2c1dace1ccded626161afc0a0600d6a1fd3f5294
vince <paulag10987@gmail.com>
2025-12-21 14:50:30 +0800

    chore(day23): add base code

--- coding_wPygame/day23.py
+++ coding_wPygame/day23.py
@@ -4,11 +4,32 @@ import sys
 from pygame.locals import *
 
 class Main:
+
+    DISPLAY_WIDTH = 800
+    DISPLAY_HEIGHT = 600
+    DISPLAY_COLOR = (0,0,0)
+
+    CLOCK = pygame.time.Clock()
+    FPS = 60
+
     def __init__(self):
-        pass
+
+        pygame.init()
+        self.DISPLAY = pygame.display.set_mode((self.DISPLAY_WIDTH, self.DISPLAY_HEIGHT))
+        pygame.display.set_caption("DAY23")
 
     def run(self):
-        pass
+
+        while True:
+            dt = self.CLOCK.tick(self.FPS) / 1000
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.DISPLAY.fill(self.DISPLAY_COLOR)
+            pygame.display.update()
 
 if __name__ == "__main__":
     app = Main()
